3bb15d17afebae16d1d6f927a37b052074403414
Cynthia Varelas <cynthiavarelas@Cynthias-iMac.local>
2021-06-12 10:51:42 -0700

    Part 3 Completed

--- deckbuilder/src/components/SearchResults.js
+++ deckbuilder/src/components/SearchResults.js
@@ -1,20 +1,24 @@
-
 import React from 'react';
 
+import Card from './Card';
 import './SearchResults.css';
 
-import Card from './Card';
-
-const SearchResults = ({ results }) => {
+const SearchResults = ({ 
+  results,
+  addCardToDeck,
+  removeCardFromDeck
+}) => {
   return (
     <div id="results">
-      <h3>
-        Here's what we found ({ results.length } results):
-      </h3>
+      <h3>Here's what we found ({ results.length } results):</h3>
       <div className="CardList">
         {
           results.map(result => (
-            <Card key={ result.id } {...result} />
+            <Card 
+              key={ result.id } 
+              addCardToDeck={ addCardToDeck }
+              removeCardFromDeck={ removeCardFromDeck }
+              {...result} />
           ))
         }
       </div>
